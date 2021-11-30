b0c1ee03a719c4091599ff27d3b1a4645474b695
Chien-Ho <chienhoo12799@gmail.com>
2021-11-30 10:43:25 +0700

    done

--- client/src/constants/authConstants.js
+++ client/src/constants/authConstants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://calm-depths-04160.herokuapp.com/api' : 'http://localhost:5000/api'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://calm-depths-04160.herokuapp.com/api'
 
 export const TOKEN_NAME = 'recipe-token'
